709b984059d92decde43108e07f2b89da23e97c6
SmarthSarin <88017689+SmarthSarin@users.noreply.github.com>
2024-08-27 23:11:10 +0530

    Updated Basic Operators

---
+++ MINER PROJECTS/Python/2_Airthmetic_Operators.py
@@ -0,0 +1,21 @@
+x=6
+y=2 
+z=x+y
+a=x*y 
+b=x/y
+c=x//y
+d=x%y
+e=x**y
+
+print ('\n' 'x =',x,
+       '\n' 'y =',y,
+    '\n' 'x + y = ',z, ', This is sum function'
+    '\n' 'x * y =',a, ', This is MULTIPLY function' 
+    '\n' 'x / y = ',b, ', This is DIVIDE function'
+    '\n' 'x // y = ',c, 'This is QUOTIENT function' 
+    '\n' 'x % y = ',d, 'This is QUOTIENT function'
+    '\n' 'x ** y = ',e, 'This is exponential function'
+
+
+),
+
