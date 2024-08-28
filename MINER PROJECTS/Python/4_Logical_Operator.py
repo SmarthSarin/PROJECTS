3145fe6f0a13a1e91f39300892a491c85a3cd042
SmarthSarin <88017689+SmarthSarin@users.noreply.github.com>
2024-08-28 11:44:51 +0530

    Added Basic Operators and Input Statements

--- MINER PROJECTS/Python/4_Logical_Operator.py
+++ MINER PROJECTS/Python/4_Logical_Operator.py
@@ -1,1 +1,38 @@
-# AND , OR , NOT
+# AND , OR , NOT 
+# Here’s the hierarchy of the logical operators:
+
+# not (Highest precedence) not true returns false , not false return true(simply for true it shows false / for false it shows true) 
+# and (if all value is true it returns true)
+# or (Lowest precedence) if one value is true it returns true
+
+a = -10
+b = -11
+g = 10
+h = 11
+d = a and b or g and h
+
+print (d) 
+
+# so in python its like we break things like that in this d = a and b or g and h
+
+# a and b = b
+
+# g and h = h 
+
+# now we have b or h 
+
+# b or h = b
+
+# answer  is b 
+
+# and b is - 11
+
+one = (10==9)
+two = (7>5)
+three = one and two 
+six = two and one
+four = one or two
+seven = two or one 
+five = not one
+eight = not two
+print (one, two, three , four , five, six, seven, eight)
---
+++ MINER PROJECTS/Python/6_Taking_Inputs.py
@@ -0,0 +1,7 @@
+a = input("enter your name")
+print ("hello,"+ a)
+#print(type(a))
+num = int(input("enter your fav number"))
+add = num +1 
+print (add)
+print (a, 'fav num is ', add)
