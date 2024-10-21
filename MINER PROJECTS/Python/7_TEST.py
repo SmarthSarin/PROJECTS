921401cc86cf1d1861592ff18b598bb2a2eb3b16
SmarthSarin <smarthsarin@gmail.com>
2024-10-21 15:03:38 +0530

    xd

---
+++ MINER PROJECTS/Python/7_TEST.py
@@ -0,0 +1,11 @@
+# cook your dish here
+# def pattern(A):
+#     for pattern in range(1,A+1):
+#         print('*'*pattern)
+        
+# pattern(5)
+
+def idk (idk):
+    for adwdwadwadwdw in range(1,1+1):
+        print('hey'*2)
+idk(5)
